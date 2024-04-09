db78ee58b52c8de1f6685ff5788fe5cdec3bd96b
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-04-09 17:03:55 +0200

    insert loader

--- frontend/src/components/blog/blog-list/BlogList.jsx
+++ frontend/src/components/blog/blog-list/BlogList.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Spinner } from "react-bootstrap";
 import BlogItem from "../blog-item/BlogItem";
 
 const BlogList = () => {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,8 +15,10 @@ const BlogList = () => {
         }
         const data = await response.json();
         setPosts(data);
+        setLoading(false); // Set loading to false when data is fetched
       } catch (error) {
         console.error("Error fetching data:", error);
+        setLoading(false); // Set loading to false in case of error
       }
     };
 
@@ -23,19 +26,27 @@ const BlogList = () => {
   }, []);
 
   return (
-    <Row>
-      {posts.map((post) => (
-        <Col
-          key={post._id}
-          md={4}
-          style={{
-            marginBottom: 50,
-          }}
-        >
-          <BlogItem {...post} />
-        </Col>
-      ))}
-    </Row>
+    <>
+      {loading ? (
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      ) : (
+        <Row>
+          {posts.map((post) => (
+            <Col
+              key={post._id}
+              md={4}
+              style={{
+                marginBottom: 50,
+              }}
+            >
+              <BlogItem {...post} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
   );
 };
 
