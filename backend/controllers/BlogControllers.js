6639b0d2dfc92908079c4357ba9cba7a55b332f5
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-04-09 15:14:44 +0200

    update

--- backend/controllers/BlogControllers.js
+++ backend/controllers/BlogControllers.js
@@ -2,74 +2,77 @@
 const BlogModel = require("../models/BlogModel");
 
 // Funzione asincrona per ottenere tutti i blog dal database e inviarli come risposta
-module.exports.getBlogs = async (req, res) => {
+module.exports.getBlogs = async (req, res, next) => {
     try {
-        // Utilizza il metodo `find` del modello Blog per recuperare tutti i blog dal database
         const blog = await BlogModel.find();
-        // Invia la lista dei blog come risposta
         res.send(blog);
     } catch (error) {
-        // Gestisce gli errori inviando un messaggio di errore e uno stato 500 al client
         console.error(error.message);
-        res.status(500).send({ error: error.message, msg: "Something went wrong!" });
+        res.status(500).send({ error: error.message, stack: error.stack, msg: "Something went wrong!" });
+        next(error);
+    } finally {
+        console.log('Blogs retrieval process completed.');
     }
 }
 
 // Funzione asincrona per ottenere i dettagli di un blog specifico in base all'ID e inviarli come risposta
-module.exports.detailBlogs = async (req, res) => {
+module.exports.detailBlogs = async (req, res, next) => {
     const { id } = req.params;
     try {
-        // Utilizza il metodo `findById` del modello Blog per recuperare un blog specifico dal database
         const blog = await BlogModel.findById(id);
-        // Invia i dettagli del blog come risposta
         res.send(blog);
     } catch (error) {
-        // Gestisce gli errori inviando un messaggio di errore e uno stato 500 al client
         console.error(error.message);
-        res.status(500).send({ error: error.message, msg: "Something went wrong!" });
+        res.status(500).send({ error: error.message, stack: error.stack, msg: "Something went wrong!" });
+        next(error);
+    } finally {
+        console.log('Blog details retrieval process completed.');
     }
 };
 
 // Funzione per salvare un nuovo blog nel database e inviare il risultato della creazione come risposta
-module.exports.saveBlogs = (req, res) => {
-    const blog = req.body;
-    // Utilizza il metodo `create` del modello Blog per salvare un nuovo blog nel database
-    BlogModel.create(blog)
-        .then((data) => {
-            // Invia il risultato della creazione come risposta con stato 201
-            console.log("Saved successfully, item: " + JSON.stringify(data));
-            res.status(201).send(data);
-        })
-        .catch((error) => {
-            // Gestisce gli errori inviando un messaggio di errore e uno stato 500 al client
-            console.error(error.message);
-            res.status(500).send({ error: error.message, msg: "Something went wrong!" });
-        });
+module.exports.saveBlogs = async (req, res, next) => {
+    try {
+        const blog = req.body;
+        const data = await BlogModel.create(blog);
+        console.log("Saved successfully, item: " + JSON.stringify(data));
+        res.status(201).send(data);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send({ error: error.message, stack: error.stack, msg: "Something went wrong!" });
+        next(error);
+    } finally {
+        console.log('Blog saving process completed.');
+    }
 }
 
 // Funzione per aggiornare un blog esistente nel database e inviare il risultato dell'aggiornamento come risposta
-module.exports.updateBlogs = (req, res) => {
+module.exports.updateBlogs = async (req, res, next) => {
     const { id } = req.params;
     const { blog } = req.body;
-    // Utilizza il metodo `findByIdAndUpdate` del modello Blog per aggiornare un blog esistente nel database
-    BlogModel.findByIdAndUpdate(id, {blog})
-        .then(() => res.send("Updated successfully, item: " + JSON.stringify(id)))
-        .catch((error) => {
-            // Gestisce gli errori inviando un messaggio di errore e uno stato 500 al client
-            console.error(error.message);
-            res.status(500).send({ error: error.message, msg: "Something went wrong!" });
-        });
+    try {
+        await BlogModel.findByIdAndUpdate(id, {blog});
+        res.send("Updated successfully, item: " + JSON.stringify(id));
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send({ error: error.message, stack: error.stack, msg: "Something went wrong!" });
+        next(error);
+    } finally {
+        console.log('Blog update process completed.');
+    }
 }
 
 // Funzione per eliminare un blog esistente dal database e inviare il risultato dell'eliminazione come risposta
-module.exports.deleteBlogs = (req, res) => {
+module.exports.deleteBlogs = async (req, res, next) => {
     const { id } = req.params;
-    // Utilizza il metodo `findByIdAndDelete` del modello Blog per eliminare un blog esistente nel database
-    BlogModel.findByIdAndDelete(id)
-        .then(() => res.send("Deleted successfully, item: " + JSON.stringify(id)))
-        .catch((error) => {
-            // Gestisce gli errori inviando un messaggio di errore e uno stato 500 al client
-            console.error(error.message);
-            res.status(500).send({ error: error.message, msg: "Something went wrong!" });
-        });
+    try {
+        await BlogModel.findByIdAndDelete(id);
+        res.send("Deleted successfully, item: " + JSON.stringify(id));
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send({ error: error.message, stack: error.stack, msg: "Something went wrong!" });
+        next(error);
+    } finally {
+        console.log('Blog deletion process completed.');
+    }
 }
