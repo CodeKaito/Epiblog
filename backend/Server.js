f391807cfd1845e4e5333a4c968f80769a8fb38f
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-04-09 14:28:44 +0200

    update

--- backend/Server.js
+++ backend/Server.js
@@ -1,30 +1,52 @@
-const express = require('express');
+const express = require('express'); // Importa il framework Express per gestire le richieste HTTP
+const mongoose = require('mongoose'); // Importa il modulo mongoose per interagire con il database MongoDB
+const cors = require('cors'); // Importa il modulo cors per interagire con il frontend
+const fs = require('fs'); // Importa il modulo fs per la lettura dei file
 
-const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config(); // Configurazione dotenv per caricare le variabili d'ambiente
 
-// const routes = require('./routes/TaskRoute');
-const routes = require('./routes/BlogRoute');
+const routes = require('./routes/BlogRoute'); // Importa le route necessarie per le chiamate HTTP
 
-const cors = require('cors');
+const PORT = process.env.PORT || 5001; // Imposta la porta del server di default a 5000, se la 5000non é disponibile allora utilizza la 5001
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const app = express(); // Crea un'app Express
+app.use(express.json()); // Middleware per il parsing del corpo della richiesta come JSON
+app.use(cors()); // Middleware per abilitare le richieste da diversi domini
 
-app.use(express.json());
-app.use(cors());
-
+// Connessione al database MongoDB utilizzando la variabile d'ambiente MONGO_URI di .env
 mongoose
     .connect(process.env.MONGO_URI)
-    .then(() => console.log('Connected to Mongodb'))
-    .catch((err) => console.log(err));
+    .then(() => console.log('Connected to Mongodb')) // Messaggio di connessione riuscita
+    .catch((err) => console.log(err)); // Gestione degli errori di connessione al database
 
-app.use("/api", routes);
+app.use("/api", routes); // Utilizza le route definite nel file BlogRoute per gli endpoint API
 
+// Route di base per controllare se il server è attivo
 app.get("/", (req, res) => {
-    res.send('This is the server response');
-})
+    // Leggi il file HTML e invialo come risposta
+    fs.readFile('./pages/Server.html', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.send(data); // Invia il contenuto del file HTML come risposta
+        }
+    });
+});
 
+app.get("/*", (req, res) => {
+    // Leggi il file HTML e invialo come risposta
+    fs.readFile('./pages/Error.html', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.send(data); // Invia il contenuto del file HTML come risposta
+        }
+    });
+});
+
+// Avvio del server sulla porta specificata
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
+    console.log(`listening on port ${PORT}`); // Consollogga la porta del server per il quale é in ascolto,
 });
