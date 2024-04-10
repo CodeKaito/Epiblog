cdef62827e9b4e5966322f124166a17084273915
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-04-10 11:40:51 +0200

    update

--- frontend/src/components/blog/blog-list/BlogList.jsx
+++ frontend/src/components/blog/blog-list/BlogList.jsx
@@ -9,7 +9,7 @@ const BlogList = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("https://epicode-api.onrender.com/api/get");
+        const response = await fetch("https://epicode-api.onrender.com/api/authors");
         if (!response.ok) {
           throw new Error("Failed to fetch data");
         }
