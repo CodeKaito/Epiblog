43b624f15fc8078705f950c7474c9816f6fa0d82
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-04-16 09:42:48 +0200

    update

--- frontend/src/components/blog/blog-list/BlogList.jsx
+++ frontend/src/components/blog/blog-list/BlogList.jsx
@@ -7,33 +7,66 @@ const BlogList = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const { searchQuery } = useSearchQuery();
+  const [noResults, setNoResults] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         let url = "https://epicode-api.onrender.com/api/blogPosts";
-
-        console.log("Sono searchQuery in BlogList: "+searchQuery);
-
+  
+        console.log("Sono searchQuery in BlogList: " + searchQuery);
+  
         // Aggiungi la query di ricerca come parametro se presente
         if (searchQuery) {
           url += `?searchTitle=${encodeURIComponent(searchQuery)}`;
         }
-
+  
         const response = await fetch(url);
         if (!response.ok) {
           throw new Error("Failed to fetch data");
         }
         const data = await response.json();
-        setPosts(data);
+  
+        // Controlla se non ci sono risultati
+        if (data.length === 0) {
+          setNoResults(true);
+        } else {
+          setNoResults(false); // Resetta il flag noResults
+          setPosts(data);
+        }
+  
         setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
         setLoading(false);
       }
     };
-
+  
     fetchData();
+  }, [searchQuery]);
+  
+  // Aggiungi questa parte per reimpostare l'elenco dei post quando la query di ricerca è vuota
+  useEffect(() => {
+    if (!searchQuery) {
+      const fetchAllPosts = async () => {
+        try {
+          const response = await fetch("https://epicode-api.onrender.com/api/blogPosts");
+          if (!response.ok) {
+            throw new Error("Failed to fetch data");
+          }
+          const data = await response.json();
+  
+          setPosts(data);
+          setNoResults(false);
+          setLoading(false);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+          setLoading(false);
+        }
+      };
+  
+      fetchAllPosts();
+    }
   }, [searchQuery]);
 
   return (
@@ -43,9 +76,15 @@ const BlogList = () => {
           <Spinner animation="border" role="status" className="loader" />
         </div>
       ) : (
-        posts.map((post) => (
-          <BlogItem key={post._id} {...post} />
-        ))
+        <>
+          {noResults ? (
+            <div className="no-results">No results</div>
+          ) : (
+            posts.map((post) => (
+              <BlogItem key={post._id} {...post} />
+            ))
+          )}
+        </>
       )}
     </>
   );
