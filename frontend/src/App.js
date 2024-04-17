a8fe2baa3210fdd47918502273044936d6c948d1
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-04-17 17:18:28 +0200

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import NotFound from "./pages/notfound/NotFound";
 import Home from "./pages/home/Home";
 import BlogDetails from "./pages/blog-details/BlogDetails";
 import Profile from "./pages/profile/Profile";
+import Posts from "./pages/posts/Posts";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/authors" element={<Author />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/posts" element={<Posts />} />
         <Route path="/details/:id" element={<BlogDetails />} />
         <Route path="/new-blog" element={<NewPost />} />
         <Route path="/*" element={<NotFound />} />
