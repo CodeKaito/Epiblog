b79de6a7793d2b6a820621d4daf291dcb0f75794
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-04-19 10:32:02 +0200

    update

--- backend/models/BlogModel.js
+++ backend/models/BlogModel.js
@@ -1,48 +1,49 @@
 // Importa il modulo mongoose per la gestione del database MongoDB
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Definisce lo schema del blog utilizzando il modulo mongoose
 const blogSchema = new mongoose.Schema({
-    // Definisce il campo 'category' di tipo String e richiesto
-    category: {
-        type: String,
-        required: true,
+  // Definisce il campo 'category' di tipo String e richiesto
+  category: {
+    type: String,
+    required: true,
+  },
+  // Definisce il campo 'title' di tipo String e richiesto
+  title: {
+    type: String,
+    required: true,
+  },
+  // Definisce il campo 'cover' di tipo String
+  cover: {
+    type: String,
+    required: false,
+  },
+  // Definisce il campo 'readTime' di tipo String e richiesto
+  readTime: {
+    value: {
+      type: Number,
+      required: true,
     },
-    // Definisce il campo 'title' di tipo String e richiesto
-    title: {
-        type: String,
-        required: true,
+    unit: {
+      type: String,
+      required: true,
     },
-    // Definisce il campo 'cover' di tipo String
-    cover: {
-        type: String,
+  },
+  author: {
+    name: {
+      type: String,
+      required: true,
     },
-    // Definisce il campo 'readTime' di tipo String e richiesto
-    readTime: {
-        value: {
-            type: Number,
-            required: true,
-        },
-        unit: {
-            type: String,
-            required: true,
-        }
+    avatar: {
+      type: String,
+      required: true,
     },
-    author: {
-        name: {
-            type: String,
-            required: true,
-        },
-        avatar: {
-            type: String,
-            required: true,
-        }
-    },
-    content: {
-        type: String,
-        required: true,
-    } 
-})
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+});
 
 // Esporta il modello mongoose associato allo schema definito sopra
 module.exports = mongoose.model("Blog", blogSchema);
